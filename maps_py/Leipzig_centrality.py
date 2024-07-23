96dbc3af2395503ad457b462fbfe7c1afe3d186b
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-07-23 20:38:52 +0200

    Update Leipzig_centrality.py

--- maps_py/Leipzig_centrality.py
+++ maps_py/Leipzig_centrality.py
@@ -1,13 +1,16 @@
-import multiprocessing as mp
-
-import numpy as np
 import osmnx as ox
+import networkx as nx
+import matplotlib.pyplot as plt
 
-np.random.seed(0)
+# Ensure you have a graph object Z loaded in your code before this step
 
-# convert graph to line graph so edges become nodes and vice versa
+# Calculate edge centrality
+Z = ox.graph_from_place("Leipzig, Saxony, Germany", network_type="drive")
 edge_centrality = nx.closeness_centrality(nx.line_graph(Z))
 nx.set_edge_attributes(Z, edge_centrality, "edge_centrality")
 
+# Get edge colors based on centrality
 ec = ox.plot.get_edge_colors_by_attr(Z, "edge_centrality", cmap="inferno")
-fig, ax = ox.plot_graph(Z, edge_color=ec, edge_linewidth=2, node_size=0)
+
+# Plot the graph
+fig, ax = ox.plot_graph(Z, edge_color=ec, edge_linewidth=2, node_size=0, bgcolor="w")
