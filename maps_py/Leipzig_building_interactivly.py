cff4c3a37ee787d0b65d18ca5c03b96b3aa839ce
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-07-24 22:05:32 +0200

    Create Leipzig_building_interactivly.py

---
+++ maps_py/Leipzig_building_interactivly.py
@@ -0,0 +1,53 @@
+import osmnx as ox
+
+# Get the buildings data for a specific place
+gdf_s = ox.features_from_place("Sued, Leipzig, Germany", tags={"building": True})
+
+# Define columns to display in the tooltip
+cols = ["height", "addr:housenumber", "addr:street", "addr:postcode"]
+
+# Explore the data using a different tile provider
+gdf_s.explore(tiles="CartoDB Positron", tooltip=cols)
+
+
+
+# Common Tile Providers:
+# OpenStreetMap
+gdf_s.explore(tiles="OpenStreetMap", tooltip=cols)
+
+# Stamen Terrain
+gdf_s.explore(tiles="Stamen Terrain", tooltip=cols)
+
+#Stamen Toner
+gdf_s.explore(tiles="Stamen Toner", tooltip=cols)
+
+#Stamen Watercolor
+gdf_s.explore(tiles="Stamen Watercolor", tooltip=cols)
+
+#CartoDB Positron
+gdf_s.explore(tiles="CartoDB Positron", tooltip=cols)
+
+#CartoDB Dark Matter
+gdf_s.explore(tiles="CartoDB Dark Matter", tooltip=cols)
+
+#OpenTopoMap
+gdf_s.explore(tiles="OpenTopoMap", tooltip=cols)
+
+# Esri World Imagery
+gdf_s.explore(tiles="Esri WorldImagery", tooltip=cols)
+
+#Other Tile Providers:
+#Esri World Street Map
+gdf_s.explore(tiles="Esri WorldStreetMap", tooltip=cols)
+
+#Esri DeLorme
+gdf_s.explore(tiles="Esri DeLorme", tooltip=cols)
+
+#Esri National Geographic
+gdf_s.explore(tiles="Esri NatGeoWorldMap", tooltip=cols)
+
+#Hike & Bike
+gdf_s.explore(tiles="HikeBike.HikeBike", tooltip=cols)
+
+#NASAGIBS.ViirsEarthAtNight2012
+gdf_s.explore(tiles="NASAGIBS.ViirsEarthAtNight2012", tooltip=cols)
