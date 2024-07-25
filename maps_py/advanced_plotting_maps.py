106b86d4ed2ee6579ed61edfb2be66cd309cf22b
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-07-25 21:41:21 +0200

    Update advanced_plotting_maps.py

--- maps_py/advanced_plotting_maps.py
+++ maps_py/advanced_plotting_maps.py
@@ -20,3 +20,84 @@ ec = ox.plot.get_edge_colors_by_attr(G, attr="length")
 
 # plot the graph with colored edges
 fig, ax = ox.plot_graph(G, node_size=5, edge_color=ec, bgcolor="w")
+
+fig, ax = ox.plot_graph(
+    G,
+    ax=None,  # optionally draw on pre-existing axis
+    figsize=(8, 8),  # figure size to create if ax is None
+    bgcolor="w",  # background color of the plot
+    node_color="pink",  # color of the nodes
+    node_size=15,  # size of the nodes: if 0, skip plotting them
+    node_alpha=None,  # opacity of the nodes
+    node_edgecolor="b",  # color of the nodes' markers' borders
+    node_zorder=1,  # zorder to plot nodes: edges are always 1
+    edge_color="#999999",  # color of the edges
+    edge_linewidth=1,  # width of the edges: if 0, skip plotting them
+    edge_alpha=None,  # opacity of the edges
+    show=True,  # if True, call pyplot.show() to show the figure
+    close=False,  # if True, call pyplot.close() to close the figure
+    save=False,  # if True, save figure to disk at filepath
+    filepath=None,  # if save is True, the path to the file
+    dpi=300,  # if save is True, the resolution of saved file
+    bbox=None,  # bounding box to constrain plot
+)
+
+Gc = ox.consolidate_intersections(ox.project_graph(G), dead_ends=True)
+c = ox.graph_to_gdfs(G, edges=False).unary_union.centroid
+bbox = ox.utils_geo.bbox_from_point(point=(c.y, c.x), dist=2200, project_utm=True)
+fig, ax = ox.plot_graph(
+    Gc,
+    figsize=(5, 5),
+    bbox=bbox,
+    edge_linewidth=2,
+    edge_color="c",
+    node_size=80,
+    node_color="#222222",
+    bgcolor="w",
+    node_edgecolor="c",
+)
+
+# or save a figure to disk instead of showing it
+fig, ax = ox.plot_graph(G, filepath="./images/image.png", save=True, show=False, close=True)
+
+# impute missing edge speeds and calculate free-flow travel times
+G = ox.add_edge_speeds(G)
+G = ox.add_edge_travel_times(G)
+
+# calculate 3 shortest paths, minimizing travel time
+w = "travel_time"
+orig, dest = list(G)[10], list(G)[-10]
+route1 = ox.shortest_path(G, orig, dest, weight=w)
+orig, dest = list(G)[0], list(G)[-1]
+route2 = ox.shortest_path(G, orig, dest, weight=w)
+orig, dest = list(G)[-100], list(G)[100]
+route3 = ox.shortest_path(G, orig, dest, weight=w)
+
+fig, ax = ox.plot_graph_route(G, route1, orig_dest_size=0, node_size=0, bgcolor="w")
+
+# you can also pass any ox.plot_graph parameters as additional keyword args
+fig, ax = ox.plot_graph_route(G, route1, save=True, show=False, close=True)
+
+routes = [route1, route2, route3]
+rc = ["r", "y", "c"]
+fig, ax = ox.plot_graph_routes(G, routes, route_colors=rc, route_linewidth=6, node_size=0, bgcolor="w")
+
+G2 = ox.graph_from_address("Leipzig, Germany", dist=500, network_type="drive")
+G2 = ox.convert.to_undirected(G2)
+
+fig, ax = ox.plot_graph(G2, edge_linewidth=3, node_size=0, show=False, close=False)
+for _, edge in ox.graph_to_gdfs(G2, nodes=False).fillna("").iterrows():
+    text = edge["name"]
+    c = edge["geometry"].centroid
+    ax.annotate(text, (c.x, c.y), c="y")
+plt.show()
+
+# get all the building footprints in a city
+gdf = ox.features_from_place("Leipzig, Germany", {"building": True})
+gdf.shape
+
+fig, ax = ox.plot_footprints(gdf, bgcolor="w", color="b")
+
+# or plot street network and the geospatial features' footprints together
+fig, ax = ox.plot_footprints(gdf, alpha=0.4, show=False)
+fig, ax = ox.plot_graph(G, ax=ax, node_size=0, edge_color="b", bgcolor="w", edge_linewidth=0.7)
