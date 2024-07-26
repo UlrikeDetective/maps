ec84963718921aab2a670925b57b5df52978d213
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-07-26 20:41:22 +0200

    Update railway.py

--- maps_py/railway.py
+++ maps_py/railway.py
@@ -13,3 +13,41 @@ Lei = ox.graph_from_place(
 )
 
 fig, ax = ox.plot_graph(Lei, node_size=0, edge_color="b", bgcolor="white", edge_linewidth=0.2)
+
+import osmnx as ox
+
+# Define the place and custom filter for railway options
+place_name = "Leipzig, Germany"
+railway_filter = '["railway"~"tram|subway|rail|light_rail|funicular|monorail|narrow_gauge|preserved|miniature"]'
+
+# Get the graph for the defined place with the specified custom filter
+Leip = ox.graph_from_place(
+    place_name,
+    retain_all=False,
+    truncate_by_edge=True,
+    simplify=True,
+    custom_filter=railway_filter,
+)
+
+# Plot the graph
+fig, ax = ox.plot_graph(SF, node_size=0, edge_color="b", bgcolor="white", edge_linewidth=0.2)
+ox.graph_to_gdfs(Leip, nodes=False).explore()
+
+import osmnx as ox
+
+# Define the place and custom filter for railway options
+place_name = "San Francisco, California, USA"
+railway_filter = '["railway"~"tram|subway|rail|light_rail|funicular|monorail|narrow_gauge|preserved|miniature"]'
+
+# Get the graph for the defined place with the specified custom filter
+SF = ox.graph_from_place(
+    place_name,
+    retain_all=False,
+    truncate_by_edge=True,
+    simplify=True,
+    custom_filter=railway_filter,
+)
+
+# Plot the graph
+fig, ax = ox.plot_graph(SF, node_size=0, edge_color="b", bgcolor="white", edge_linewidth=0.2)
+ox.graph_to_gdfs(SF, nodes=False).explore()
