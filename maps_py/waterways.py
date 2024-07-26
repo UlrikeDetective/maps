6146c564468738498a28b28acd676c3570130263
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-07-26 20:50:08 +0200

    Update waterways.py

--- maps_py/waterways.py
+++ maps_py/waterways.py
@@ -19,3 +19,41 @@ Leip = ox.graph_from_place(
 # Plot the graph
 fig, ax = ox.plot_graph(Leip, node_size=0, edge_color="b", bgcolor="white", edge_linewidth=0.2)
 ox.graph_to_gdfs(Leip, nodes=False).explore()
+
+import osmnx as ox
+
+# Define the place and custom filter for railway options
+place_name = "Amsterdam, The Netherlands"
+waterway_filter = '["waterway"~"canal|river|ditch|stream|drain"]'
+
+# Get the graph for the defined place with the specified custom filter
+Amst = ox.graph_from_place(
+    place_name,
+    retain_all=False,
+    truncate_by_edge=True,
+    simplify=True,
+    custom_filter=waterway_filter,
+)
+
+# Plot the graph
+fig, ax = ox.plot_graph(Amst, node_size=0, edge_color="b", bgcolor="white", edge_linewidth=0.2)
+ox.graph_to_gdfs(Amst, nodes=False).explore()
+
+import osmnx as ox
+
+# Define the place and custom filter for railway options
+place_name = "Jakarta, Indonesia"
+waterway_filter = '["waterway"~"canal|river|ditch|stream|drain"]'
+
+# Get the graph for the defined place with the specified custom filter
+Jak = ox.graph_from_place(
+    place_name,
+    retain_all=False,
+    truncate_by_edge=True,
+    simplify=True,
+    custom_filter=waterway_filter,
+)
+
+# Plot the graph
+fig, ax = ox.plot_graph(Jak, node_size=0, edge_color="b", bgcolor="white", edge_linewidth=0.2)
+ox.graph_to_gdfs(Jak, nodes=False).explore()
