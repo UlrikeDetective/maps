ec6ce3f87c97871dff99c3d0c885e0eb7e2be4f2
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-07-26 21:01:48 +0200

    Update railway.py

--- maps_py/railway.py
+++ maps_py/railway.py
@@ -51,3 +51,22 @@ SF = ox.graph_from_place(
 # Plot the graph
 fig, ax = ox.plot_graph(SF, node_size=0, edge_color="b", bgcolor="white", edge_linewidth=0.2)
 ox.graph_to_gdfs(SF, nodes=False).explore()
+
+import osmnx as ox
+
+# Define the place and custom filter for railway options
+place_name = "London, UK"
+railway_filter = '["railway"~"tram|subway|rail|light_rail|funicular|monorail|narrow_gauge|preserved|miniature"]'
+
+# Get the graph for the defined place with the specified custom filter
+Lon = ox.graph_from_place(
+    place_name,
+    retain_all=False,
+    truncate_by_edge=True,
+    simplify=True,
+    custom_filter=railway_filter,
+)
+
+# Plot the graph
+fig, ax = ox.plot_graph(Lon, node_size=0, edge_color="b", bgcolor="white", edge_linewidth=0.2)
+ox.graph_to_gdfs(Lon, nodes=False).explore()
