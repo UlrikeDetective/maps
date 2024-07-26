6c8ee1b91f708ba60b409c3482c937dfa6580c96
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-07-26 21:10:46 +0200

    Update osmnx_filters.py

--- maps_py/osmnx_filters.py
+++ maps_py/osmnx_filters.py
@@ -21,3 +21,69 @@ Leip = ox.graph_from_place(
 # Plot the graph
 fig, ax = ox.plot_graph(Leip, node_size=0, edge_color="b", bgcolor="white", edge_linewidth=0.2)
 ox.graph_to_gdfs(Leip, nodes=False).explore()
+
+import osmnx as ox
+
+# Define the place and custom filter for railway options
+place_name = "Leipzig, Germany"
+amenity_filter = '["amenity"~"school|hospital|restaurant|bar|cafe|bank|post_office|police|fire_station"]'
+
+# Get the graph for the defined place with the specified custom filter
+Leip = ox.graph_from_place(
+    place_name,
+    retain_all=False,
+    truncate_by_edge=True,
+    simplify=True,
+    custom_filter=amenity_filter,
+)
+
+# Plot the graph
+fig, ax = ox.plot_graph(Leip, node_size=0, edge_color="b", bgcolor="white", edge_linewidth=0.2)
+ox.graph_to_gdfs(Leip, nodes=False).explore()
+
+import osmnx as ox
+
+# Define the place and custom filter for railway options
+place_name = "Leipzig, Germany"
+building_filter = '["building"]'
+
+# Get the graph for the defined place with the specified custom filter
+Leip = ox.graph_from_place(
+    place_name,
+    retain_all=False,
+    truncate_by_edge=True,
+    simplify=True,
+    custom_filter=building_filter,
+)
+
+# Plot the graph
+fig, ax = ox.plot_graph(Leip, node_size=0, edge_color="b", bgcolor="white", edge_linewidth=0.2)
+ox.graph_to_gdfs(Leip, nodes=False).explore()
+
+import osmnx as ox
+
+# Define the place
+place_name = "San Francisco, California, USA"
+
+# Define custom filters
+highway_filter = '["highway"~"motorway|trunk|primary|secondary|tertiary|residential|unclassified|service"]'
+amenity_filter = '["amenity"~"school|hospital|restaurant|bar|cafe|bank|post_office|police|fire_station"]'
+building_filter = '["building"]'
+landuse_filter = '["landuse"~"residential|commercial|industrial|forest|farmland|meadow"]'
+natural_filter = '["natural"~"wood|water|beach|coastline|grassland|tree"]'
+leisure_filter = '["leisure"~"park|pitch|playground|sports_centre|swimming_pool|garden"]'
+shop_filter = '["shop"~"supermarket|convenience|clothes|bakery|pharmacy"]'
+tourism_filter = '["tourism"~"hotel|museum|gallery|attraction|zoo|theme_park"]'
+
+# Get the data using filters
+G_highways = ox.graph_from_place(place_name, custom_filter=highway_filter)
+amenities = ox.features_from_place(place_name, tags={"amenity": True})
+buildings = ox.features_from_place(place_name, tags={"building": True})
+landuse = ox.features_from_place(place_name, tags={"landuse": True})
+natural_features = ox.features_from_place(place_name, tags={"natural": True})
+leisure = ox.features_from_place(place_name, tags={"leisure": True})
+shops = ox.features_from_place(place_name, tags={"shop": True})
+tourism = ox.features_from_place(place_name, tags={"tourism": True})
+
+# Plot the data (example for highways)
+fig, ax = ox.plot_graph(G_highways, node_size=0, edge_color="b", bgcolor="white", edge_linewidth=0.2)
