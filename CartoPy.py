1720ea1e90b323523087ee2e527bf6840ee62480
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-07-28 10:52:20 +0200

    Update CartoPy.py

--- CartoPy.py
+++ CartoPy.py
@@ -32,3 +32,27 @@ axs[0].add_geometries(
 )
 # Make the GeoPandas plot
 df_epsg.plot(ax=axs[1], color="white", edgecolor="black")
+
+crs_new = ccrs.AlbersEqualArea()
+new_geometries = [
+    crs_new.project_geometry(ii, src_crs=crs) for ii in df_ae["geometry"].values
+]
+
+fig, ax = plt.subplots(subplot_kw={"projection": crs_new})
+ax.add_geometries(new_geometries, crs=crs_new)
+
+df_aea = geopandas.GeoDataFrame(
+    df.drop(columns="geometry"), geometry=new_geometries, crs=crs_new.proj4_init
+)
+df_aea.plot()
+
+# Generate a CartoPy figure and add the countries to it
+fig, ax = plt.subplots(subplot_kw={"projection": crs_new})
+ax.add_geometries(new_geometries, crs=crs_new)
+
+# Calculate centroids and plot
+df_aea_centroids = df_aea.geometry.centroid
+# Need to provide "zorder" to ensure the points are plotted above the polygons
+df_aea_centroids.plot(ax=ax, markersize=5, color="r", zorder=10)
+
+plt.show()
