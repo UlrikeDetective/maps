22a926861ae9af6b8b95f15315dfffad3f4ee089
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-07-29 21:17:13 +0200

    Update Folium_volcano.py

--- Folium/Folium_volcano.py
+++ Folium/Folium_volcano.py
@@ -17,3 +17,81 @@ geo_df = geopandas.GeoDataFrame(
 )
 
 geo_df.head()
+
+world = geopandas.read_file(geodatasets.get_path("naturalearth.land"))
+df.Type.unique()
+
+fig, ax = plt.subplots(figsize=(24, 18))
+world.plot(ax=ax, alpha=0.4, color="grey")
+geo_df.plot(column="Type", ax=ax, legend=True)
+plt.title("Volcanoes")
+
+fig, ax = plt.subplots(figsize=(24, 18))
+world.plot(ax=ax, alpha=0.4, color="grey")
+geo_df.plot(column="Type", ax=ax, legend=True)
+plt.title("Volcanoes")
+
+# Maps Openstreetmap, CartoDB Positron or Mapbox
+# CartoDB Positron Japan
+map = folium.Map(location=[35.653, 139.839], tiles="CartoDB Positron", zoom_start=6)
+map
+
+map = folium.Map(location=[35.653, 139.839], tiles="OpenStreetMap", zoom_start=6)
+map
+
+# Create a geometry list from the GeoDataFrame
+geo_df_list = [[point.xy[1][0], point.xy[0][0]] for point in geo_df.geometry]
+
+# Iterate through list and add a marker for each volcano, color-coded by its type.
+i = 0
+for coordinates in geo_df_list:
+    # assign a color marker for the type of volcano, Strato being the most common
+    if geo_df.Type[i] == "Stratovolcano":
+        type_color = "green"
+    elif geo_df.Type[i] == "Complex volcano":
+        type_color = "blue"
+    elif geo_df.Type[i] == "Shield volcano":
+        type_color = "orange"
+    elif geo_df.Type[i] == "Lava dome":
+        type_color = "pink"
+    else:
+        type_color = "purple"
+
+    # Place the markers with the popup labels and data
+    map.add_child(
+        folium.Marker(
+            location=coordinates,
+            popup="Year: "
+            + str(geo_df.Year[i])
+            + "<br>"
+            + "Name: "
+            + str(geo_df.Name[i])
+            + "<br>"
+            + "Country: "
+            + str(geo_df.Country[i])
+            + "<br>"
+            + "Type: "
+            + str(geo_df.Type[i])
+            + "<br>"
+            + "Coordinates: "
+            + str(geo_df_list[i]),
+            icon=folium.Icon(color="%s" % type_color),
+        )
+    )
+    i = i + 1
+
+map
+
+# This example uses heatmaps to visualize the density of volcanoes
+# which is more in some parts of the world compared to others.
+
+from folium import plugins
+
+map = folium.Map(location=[15, 30], tiles="Cartodb dark_matter", zoom_start=2)
+
+heat_data = [[point.xy[1][0], point.xy[0][0]] for point in geo_df.geometry]
+
+heat_data
+plugins.HeatMap(heat_data).add_to(map)
+
+map
