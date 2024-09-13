6b5b271aa4182a075ff36c20f7c4e1da9b661cf2
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-09-13 18:49:02 +0200

    google_maps_app

---
+++ Json_files/google_maps_app.js
@@ -0,0 +1,11 @@
+function sendMap() {
+    var sheet = SpreadsheetApp.getActiveSheet();
+    var address = sheet.getRange("A1").getValue();
+    var map = Maps.newStaticMap().addMarker(address);
+    GmailApp.sendEmail("<YOUR_EMAIL>", "Map", 'See below.', {attachments:[map]});
+}
+
+/**
+* @OnlyCurrentDoc
+*/
+
