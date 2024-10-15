a5ec8671c0f117020c8b2971ea5d4aef60fedd13
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-10-15 20:41:03 +0200

    Leipzig Wiederitzsch

--- OSMnx/maps_py/Leipzig_suburbs.py
+++ OSMnx/maps_py/Leipzig_suburbs.py
@@ -4,5 +4,5 @@ import numpy as np
 import osmnx as ox
 
 # download/model a street network for some city then visualize it
-Holz = ox.graph_from_place("Holzhausen, Suedost, Leipzig, Saxony, Germany", network_type="drive")
+Holz = ox.graph_from_place("Wiederitzsch, Nord, Leipzig, Saxony, Germany", network_type="drive")
 fig, ax = ox.plot_graph(Holz, node_size=0.7, bgcolor="w", edge_color="b")
