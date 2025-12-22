f95a5380a0c557dbc0f1ee752bd0c3801aee6a8f
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2025-12-22 13:08:17 +0100

    connection lines V03 V02

--- app/map_pointsV01/connectionPointsV03.py
+++ app/map_pointsV01/connectionPointsV03.py
@@ -14,8 +14,8 @@ def save_image(coordinates, cities):
     fig, ax = plt.subplots(figsize=(8, 6))
     lat, lon = zip(*coordinates)
 
-    ax.scatter(lon, lat, color="#B77651", zorder=5)
-    ax.plot(lon, lat, color="#01393D", linewidth=3, zorder=3)
+    ax.scatter(lon, lat, color="#B77651", zorder=10)
+    ax.plot(lon, lat, color="#01393D", linewidth=3, linestyle=":", zorder=3)
 
     texts = []
     for i, city in enumerate(cities):
@@ -26,17 +26,22 @@ def save_image(coordinates, cities):
                 city,
                 fontsize=14,
                 fontweight="bold",
-                bbox=dict(facecolor="#58989F", edgecolor="none", alpha=0.85, pad=0.3),
-                zorder=10
+                bbox=dict(facecolor="#58989F", edgecolor="none", alpha=0.55, pad=0.9),
+                zorder=12
             )
         )
 
     adjust_text(
         texts,
         ax=ax,
-        arrowprops=dict(arrowstyle="-", color="#01393D", linewidth=1),
-        expand_points=(1.2, 1.4),
-        expand_text=(1.2, 1.4)
+        expand_points=(2.5, 2.5), # distance from points
+        expand_text=(2.0, 2.0), # distance between labels
+        force_points=0.6, # strength of repulsion from points
+        force_text=0.6, # strength of repulsion from other labels
+        arrowprops=dict(
+            arrowstyle="-",
+            color="#01393D",
+            linewidth=1)
     )
 
     ax.set_facecolor("#58989F")
